f34cbab2de36f99b179095dd19bad36e42ae0092
clarodanesh <44497069+clarodanesh@users.noreply.github.com>
2019-08-30 23:08:06 +0100

    search activity now sets date and time
    search activity now implements date and time picker dialogs which allow date and time to be set to textviews, ALSO colour changed for accents to the metallic gold

--- app/src/main/java/com/riderrapp/riderr/SearchActivity.java
+++ app/src/main/java/com/riderrapp/riderr/SearchActivity.java
@@ -24,8 +24,21 @@ import androidx.appcompat.app.ActionBar;
 import android.view.Menu;
 import android.widget.Button;
 
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.widget.TextView;
 
-public class SearchActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class SearchActivity extends AppCompatActivity implements
+        View.OnClickListener{
+
+    Button btnDatePicker, btnTimePicker;
+    TextView txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +50,67 @@ public class SearchActivity extends AppCompatActivity {
         final Intent intent = new Intent(this, MainActivity.class);
 
 
-        final Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        final Button searchRideBtn = (Button) findViewById(R.id.searchRideBtn);
+        searchRideBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Code here executes on main thread after user presses button
                 startActivity(intent);
             }
         });
+
+        btnDatePicker=(Button)findViewById(R.id.btn_date);
+        btnTimePicker=(Button)findViewById(R.id.btn_time);
+        txtDate=(TextView) findViewById(R.id.in_date);
+        txtTime=(TextView) findViewById(R.id.in_time);
+
+        btnDatePicker.setOnClickListener(this);
+        btnTimePicker.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnDatePicker) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            mYear = c.get(Calendar.YEAR);
+            mMonth = c.get(Calendar.MONTH);
+            mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == btnTimePicker) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            mHour = c.get(Calendar.HOUR_OF_DAY);
+            mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            txtTime.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
     }
 }
