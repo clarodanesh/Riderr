2b8ebf59714bf7bf4261904c359ce4f79a9628f0
clarodanesh <44497069+clarodanesh@users.noreply.github.com>
2019-09-04 22:28:51 +0100

    offer ride activity front end completed`
    offer ride front end code completed now just need to implement back end code for it

--- app/src/main/java/com/riderrapp/riderr/OfferRideActivity.java
+++ app/src/main/java/com/riderrapp/riderr/OfferRideActivity.java
@@ -24,8 +24,22 @@ import androidx.appcompat.app.ActionBar;
 import android.view.Menu;
 import android.widget.Button;
 
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.widget.TextView;
 
-public class OfferRideActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class OfferRideActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //creating variables for buttons, textview and integers to use
+    Button pickDateBtn, pickTimeBtn;
+    TextView dateText, timeText;
+    private int theYear, theMonth, theDay, theHour, theMinute;
+    private String fullDate, fullTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +47,79 @@ public class OfferRideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_offer_ride);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        final Button offerRideBtn = (Button) findViewById(R.id.offerRideBtn);
+        offerRideBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                //startActivity(intent);
+
+                //here can onclick get the fulldate and time and onlclick send to server
+                //dateText.setText(fullDate);
+            }
+        });
+
+        //get the views and assign to the buttons and text view variables
+        pickDateBtn=(Button)findViewById(R.id.btn_offer_ride_date);
+        pickTimeBtn=(Button)findViewById(R.id.btn_offer_ride_time);
+        dateText=(TextView) findViewById(R.id.offer_ride_date);
+        timeText=(TextView) findViewById(R.id.offer_ride_time);
+
+        //set on click listeners for the buttons
+        pickDateBtn.setOnClickListener(this);
+        pickTimeBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //if the view is the pick date button then handle it here
+        if (v == pickDateBtn) {
+
+            // need to get the year month and day of the date
+            final Calendar calendar = Calendar.getInstance();
+            theYear = calendar.get(Calendar.YEAR);
+            theMonth = calendar.get(Calendar.MONTH);
+            theDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+            // handle date picker dialogue here by launching it
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                            //dateText.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            fullDate = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                            pickDateBtn.setText(fullDate);
+                            //dateText.setText(fullDate);
+                        }
+                    }, theYear, theMonth, theDay);
+            datePickerDialog.show();
+        }
+
+        //if the view being clicked is the pick time button then handle it here
+        if (v == pickTimeBtn) {
+
+            // need to get the hour and minute of current time
+            final Calendar calendar = Calendar.getInstance();
+            theHour = calendar.get(Calendar.HOUR_OF_DAY);
+            theMinute = calendar.get(Calendar.MINUTE);
+
+            // handle timpicker dialogue by launching it
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            //timeText.setText(hourOfDay + ":" + minute);
+                            fullTime = hourOfDay + ":" + minute;
+                            pickTimeBtn.setText(fullTime);
+                        }
+                    }, theHour, theMinute, false);
+            timePickerDialog.show();
+        }
     }
 }
