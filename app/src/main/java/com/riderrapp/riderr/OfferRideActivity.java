32e2a373a49945ca1320919b37ee1a891921b031
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-01-02 01:16:58 +0000

    Rides can now be offered (still some refining to do)
    Rides can now be offered to users, still need to do some refining such as only allowing users who have set their car details to be able to do this

--- app/src/main/java/com/riderrapp/riderr/OfferRideActivity.java
+++ app/src/main/java/com/riderrapp/riderr/OfferRideActivity.java
@@ -1,19 +1,46 @@
 package com.riderrapp.riderr;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -33,8 +60,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OfferRideActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "OfferRideActivity";
 
     //creating variables for buttons, textview and integers to use
     Button pickDateBtn, pickTimeBtn;
@@ -42,12 +74,29 @@ public class OfferRideActivity extends AppCompatActivity implements View.OnClick
     private int theYear, theMonth, theDay, theHour, theMinute;
     private String fullDate, fullTime, destination;
 
+    //Mapbox places plugin implementation
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private CarmenFeature home;
+    private CarmenFeature work;
+    private String geojsonSourceLayerId = "geojsonSourceLayerId";
+    private String symbolIconId = "symbolIconId";
+    private String country, dateTimeStamp, offeredBy, place, region, placeName;
+    private double longitude, latitude;
+    private int vehicleCapacity;
+
+
+    //String c, double lng, double lt, String dt, String stmp, String dst, String off, String pl, String rg, String t, int vcap
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offer_ride);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+        Mapbox.getInstance(this, getString(R.string.access_token));
+
 
         final Button offerRideBtn = (Button) findViewById(R.id.offerRideBtn);
         offerRideBtn.setOnClickListener(new View.OnClickListener() {
@@ -57,10 +106,10 @@ public class OfferRideActivity extends AppCompatActivity implements View.OnClick
 
                 //here can onclick get the fulldate and time and onclick send to server
                 //dateText.setText(fullDate);
-                destination = "";
+
                 //send the data for the ride from here
                 if(IsDataFilled(fullDate, fullTime, destination)){
-                    StoreData();
+                    StoreData(country, longitude, latitude, fullDate, dateTimeStamp, destination, offeredBy, place, region, fullTime, vehicleCapacity, placeName);
                 }
                 else{
                     Toast.makeText(OfferRideActivity.this, "Fill the entire form before trying to submit",
@@ -77,11 +126,14 @@ public class OfferRideActivity extends AppCompatActivity implements View.OnClick
 
         fullDate = "";
         fullTime = "";
+        destination = "";
 
         //set on click listeners for the buttons
         pickDateBtn.setOnClickListener(this);
         pickTimeBtn.setOnClickListener(this);
 
+        GetUserDetails();
+        InitDestinationSearch();
     }
 
     @Override
@@ -137,6 +189,85 @@ public class OfferRideActivity extends AppCompatActivity implements View.OnClick
         }
     }
 
+    private void InitDestinationSearch() {
+        findViewById(R.id.offerRideTxtBox).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new PlaceAutocomplete.IntentBuilder() //this intent will open the activity that shows search results, will set up as a start activity for result, meaning it will close after result
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.access_token))
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#FFFFFF"))
+                                .limit(10)
+                                /*.addInjectedFeature(home)
+                                .addInjectedFeature(work)*/
+                                .build(PlaceOptions.MODE_FULLSCREEN))
+                        .build(OfferRideActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+            EditText offerRideTxtBox = (EditText)findViewById(R.id.offerRideTxtBox);
+
+            // Retrieve selected location's CarmenFeature
+            CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
+            //String add1 = PlaceAutocomplete.getPlace(data).placeName();
+            destination = PlaceAutocomplete.getPlace(data).text();
+            LatLng latlng = new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
+                    ((Point) selectedCarmenFeature.geometry()).longitude());
+            latitude = latlng.getLatitude();
+            longitude = latlng.getLongitude();
+            placeName = PlaceAutocomplete.getPlace(data).placeName();
+
+            offerRideTxtBox.setText(destination);
+
+            //to access the carmen feature selectedCarmenFeature.context().get(1).id or .text and whatnot;
+            if(selectedCarmenFeature.context().size() > 0){
+                for(int i = 0; i < selectedCarmenFeature.context().size(); i++){
+                    if(selectedCarmenFeature.context().get(i).id().contains("region")){
+                        region = selectedCarmenFeature.context().get(i).text();
+                    }
+                    if(selectedCarmenFeature.context().get(i).id().contains("country")){
+                        country = selectedCarmenFeature.context().get(i).text();
+                    }
+                    if(selectedCarmenFeature.context().get(i).id().contains("place")){
+                        place = selectedCarmenFeature.context().get(i).text();
+                    }else{
+                        place = destination;
+                    }
+                }
+            }
+
+
+            //ed.setText(region);
+            //lt.setText(ctry);
+            //lg.setText(plce);
+
+            Date dtStamp = Calendar.getInstance().getTime();
+            dateTimeStamp = dtStamp.toString();
+
+            /*
+            Firebase.upload(
+                string country;
+                double long and lat;
+                string date
+                string dateTime stamp
+                string destination
+                string offeredBy : uuid
+                string place
+                string region
+                string time
+                int vehicleCapacity
+            )
+            * */
+
+        }
+    }
+
     private boolean IsDataFilled(String d, String t, String dest){
         if(d == "" || d.isEmpty()){
             return false;
@@ -150,7 +281,64 @@ public class OfferRideActivity extends AppCompatActivity implements View.OnClick
         return true;
     }
 
-    private void StoreData(){
+    private void GetUserDetails(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser fbuser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = fbuser.getUid();
+        offeredBy = uid;
 
+        DocumentReference docRef = db.collection("users").document(uid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getString("car-make"));
+
+                        vehicleCapacity = document.getLong("seats-no").intValue();
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void StoreData(String c, double lng, double lt, String dt, String stmp, String dst, String off, String pl, String rg, String t, int vcap, String pName){
+        //SEND EVERYTHING TO SERVER
+        //FOR used id and vehicle capacity create functions to get them from firebase
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("country", c);
+        data.put("date", dt);
+        data.put("dateTimeStamp", stmp);
+        data.put("destination", dst);
+        data.put("latitude", lt);
+        data.put("longitude", lng);
+        data.put("offeredBy", off);
+        data.put("place", pl);
+        data.put("region", rg);
+        data.put("time", t);
+        data.put("vehicleCapacity", vcap);
+        data.put("placeName", pName);
+
+        db.collection("OfferedRides")
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }
