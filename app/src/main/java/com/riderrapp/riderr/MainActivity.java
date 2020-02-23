03b64edc02917abb9469849b7d47f8354c20a608
Danesh Iqbal <44497069+clarodanesh@users.noreply.github.com>
2020-02-23 01:44:52 +0000

    main activity map shows destination of journey if user has one

--- app/src/main/java/com/riderrapp/riderr/MainActivity.java
+++ app/src/main/java/com/riderrapp/riderr/MainActivity.java
@@ -3,14 +3,18 @@ package com.riderrapp.riderr;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 
@@ -18,6 +22,10 @@ import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
@@ -30,6 +38,7 @@ import com.mapbox.mapboxsdk.plugins.annotation.Symbol;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager;
 import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonOptions;
+import com.mapbox.mapboxsdk.utils.BitmapUtils;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -45,10 +54,14 @@ import timber.log.Timber;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private MapView mapView;
+    private static final String TAG = "MainActivity";
+
+    private MapView mainMapView;
     private SymbolManager symbolManager;
     private Symbol symbol;
     private static final String ID_ICON_MARKER = "marker";
+    private long lat = 0, lng = 0;
+    private String pride = "", dride = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,14 +149,46 @@ public class MainActivity extends AppCompatActivity
             }
         });*/
 
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mainMapView = findViewById(R.id.mapView);
+        mainMapView.onCreate(savedInstanceState);
+        mainMapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull final MapboxMap mapboxMap) {
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
-                    public void onStyleLoaded(@NonNull Style style) {
+                    public void onStyleLoaded(@NonNull final Style style) {
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        FirebaseUser fbuser = FirebaseAuth.getInstance().getCurrentUser();
+                        String uid = fbuser.getUid();
+
+                        DocumentReference docRef = db.collection("users").document(uid);
+                        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    DocumentSnapshot document = task.getResult();
+                                    if (document.exists()) {
+                                        pride = document.getString("p-ride");
+                                        //dride = document.getLong("longitude");
+                                        dride = document.getString("d-ride");
+
+                                        if(dride != null) {
+                                            SetRide(style, mapboxMap, dride);
+                                        }else if(pride != null){
+                                            SetRide(style, mapboxMap, pride);
+                                        }else{
+
+                                        }
+
+
+                                    } else {
+                                        Log.d(TAG, "No such document");
+                                    }
+                                } else {
+                                    Log.d(TAG, "get failed with ", task.getException());
+                                }
+                            }
+                        });
                     }
                 });
             }
@@ -220,45 +265,102 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    private void SetRide(final Style style, final MapboxMap mapboxMap, String rideid){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseUser fbuser = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = fbuser.getUid();
+
+        DocumentReference docRef = db.collection("OfferedRides").document(rideid);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        lat = document.getLong("latitude");
+                        //dride = document.getLong("longitude");
+                        lng = document.getLong("longitude");
+
+
+                        addAirplaneImageToStyle(style);
+                        // create symbol manager
+                        GeoJsonOptions geoJsonOptions = new GeoJsonOptions().withTolerance(0.4f);
+                        symbolManager = new SymbolManager(mainMapView, mapboxMap, style, null, geoJsonOptions);
+
+
+                        // set non data driven properties
+                        symbolManager.setIconAllowOverlap(true);
+                        symbolManager.setTextAllowOverlap(true);
+
+                        // create a symbol
+                        SymbolOptions symbolOptions = new SymbolOptions()
+                                .withLatLng(new LatLng(lat, lng))
+                                .withIconImage(ID_ICON_MARKER)
+                                .withIconSize(1f)
+                                .withSymbolSortKey(10.0f);
+                        //.withDraggable(true);
+                        symbol = symbolManager.create(symbolOptions);
+                        Timber.e(symbol.toString());
+
+                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                                new CameraPosition.Builder()
+                                        .target(new LatLng(lat, lng))
+                                        .zoom(8)
+                                        .build()), 4000);
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void addAirplaneImageToStyle(Style style) {
+        style.addImage(ID_ICON_MARKER,
+                BitmapUtils.getBitmapFromDrawable(ContextCompat.getDrawable(this, R.drawable.ic_riderr_launcher_foreground)));
+    }
+
     @Override
     public void onStart() {
         super.onStart();
-        mapView.onStart();
+        mainMapView.onStart();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
+        mainMapView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mapView.onPause();
+        mainMapView.onPause();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mapView.onStop();
+        mainMapView.onStop();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mapView.onLowMemory();
+        mainMapView.onLowMemory();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mapView.onDestroy();
+        mainMapView.onDestroy();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
+        mainMapView.onSaveInstanceState(outState);
     }
 }
